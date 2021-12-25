6598127c7fd4772f91702a9d1123df7c834baf24
Nguyễn Phi ANH <90373010+anhcoder03@users.noreply.github.com>
2021-12-25 12:13:15 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function () {
+    const anh = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+
+    // click --> 3 gạch --> add class vào.
+    anh && anh.addEventListener("click", inMenu);
+    function inMenu(event) {
+    menu && menu.classList.add("is-show");
+    }
+    // click -- ngoài menu --> remove class
+    document.addEventListener("click", outInMenu);
+    function outInMenu(event) {
+    if (event.target.matches(".menu-toggle") || event.target.matches(".menu"))
+    return;
+    menu && menu.classList.remove("is-show");
+    }
+});
